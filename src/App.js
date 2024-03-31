adcc9a1faa9e92a76da33072a1dce99d25999010
thang2603 <thangc2k53@gmail.com>
2024-03-31 09:54:02 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import bacground from "./assets/HG13.png";
+import "./App.css";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <img src={bacground} className="App-logo" alt="logo" />
       </header>
     </div>
   );
